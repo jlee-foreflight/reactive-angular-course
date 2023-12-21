213ffebbf20733d39aec814bb087c9be9853a60a
Jared Lee <jlee@foreflight.com>
2023-12-20 16:13:25 -0800

    End of section 6

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -12,6 +12,26 @@ export class CoursesService {
   constructor(private http: HttpClient) {
   }
 
+  loadCourseById(courseId: number) {
+    return this.http.get<Course>('/api/courses/' + courseId)
+        .pipe(
+            // map(res => res["payload"]),
+            shareReplay()
+        );
+  }
+
+  loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http.get<Lesson[]>('/api/lessons', {
+      params: {
+        pageSize: "10000",
+        courseId: courseId.toString(),
+      }
+    }).pipe(
+        map(res => res["payload"]),
+        shareReplay()
+    );
+  }
+
   loadAllCourses(): Observable<Course[]> {
     return this.http.get<Course[]>("/api/courses")
       .pipe(
